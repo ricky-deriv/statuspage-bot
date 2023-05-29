7863bce31def6dab8d6a8f89b76ceba8487928a2
ricky-deriv <ricky-deriv@deriv.com>
2023-05-29 13:22:37 +0800

    optimize codes

--- lib/app.py
+++ lib/app.py
@@ -34,55 +34,60 @@ def handle_app_mention_events(body, say, client):
 def declare_incident(ack, shortcut, client):
     ack()
     channel_id = shortcut['channel']['id']
+
     with open('template/incident-form.json') as file:
         form_create_incident = json.load(file)
     form_create_incident['private_metadata'] = channel_id
 
-    if(check_allowed_trigger(shortcut['channel']['name'], shortcut['user']['id'], shortcut['message']['text'])):
-        print('yes, youre allwoed')
-        # access the view object and update the dropbox for incident status
-        index = 0
+    if(check_allowed_trigger(shortcut['channel']['name'], shortcut['user']['id'], shortcut['message']['text'])):        
+         # Update the dropbox for incident status
         for block in form_create_incident['blocks']:
-            if ('accessory' in block):
-                if (block['accessory']['action_id'] == 'static_select_action'):
-                    for status in INCIDENT_STATUSES:
-                        form_create_incident['blocks'][index]['accessory']['options'].append({
-                            "text": {"type": "plain_text", "text": status},
-                            "value": status
-                        })
-            index += 1
+            accessory = block.get('accessory')
+            if accessory and accessory.get('action_id') == 'static_select_action':
+                block['accessory']['options'].extend([
+                    {
+                        "text": {"type": "plain_text", "text": status},
+                        "value": status
+                    }
+                    for status in INCIDENT_STATUSES
+                ])
         # send the form
         client.views_open(
             trigger_id=shortcut["trigger_id"],
             view=form_create_incident
         )
     else:
-        print('nope, not allowed')
         with open('template/not-allowed.json') as file:
             not_allowed = json.load(file)
         client.views_open(
-        trigger_id=shortcut["trigger_id"],
-        view=not_allowed
-    )
+            trigger_id=shortcut["trigger_id"],
+            view=not_allowed
+        )
 
 @app.view("form_create_incident")
 def post_incident(ack, body, client, view, say):
     ack()
-    incident_name = view["state"]["values"]["incident_name_input"]["incident_name_input"]['value']
-    incident_status = view['state']['values']['static_select_action']['static_select_action']['selected_option']['text']['text']
-    incident_description = view['state']['values']['description_input']['description_input']['value']
+    state_values = view["state"]["values"]
+    
+    incident_name = state_values["incident_name_input"]["incident_name_input"]["value"]
+    incident_status = state_values["static_select_action"]["static_select_action"]["selected_option"]["text"]["text"]
+    incident_description = state_values["description_input"]["description_input"]["value"]
     channel_id = view["private_metadata"]
+    
     message = create_incident(incident_name, incident_status, incident_description)
-    say(f"{message}", channel = channel_id)
+    say(message, channel=channel_id)
 
 def check_allowed_trigger(incident_name, slack_user_id, message):
-    # allow operation if 
-    #   channel name starts with incident
-    #   in the allowed user list
-    #   message contains the keywords
-    allowed_slack_users_id = ['U056F2PDN3G']
+    """
+     allow operation if 
+        - channel name starts with incident
+        - in the allowed user list
+        - message contains the keywords
+    """
+    allowed_slack_users_id = {'U056F2PDN3G'}
     key_string = 'statuspage declare incident'
-    return True if (incident_name.startswith('incident') and slack_user_id in allowed_slack_users_id and message == key_string) else False
+    
+    return incident_name.startswith('incident') and slack_user_id in allowed_slack_users_id and message == key_string
     
     
 
